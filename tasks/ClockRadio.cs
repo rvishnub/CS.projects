4b7da378bdc9ed4f7ccb4e4fb9f7f7dc8bd1a38f
rvishnub <rvishnub@yahoo.com>
2016-08-08 14:41:43 -0500

    re-saved all files in the tasks folder

--- tasks/ClockRadio.cs
+++ tasks/ClockRadio.cs
@@ -4,12 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace ClockRadio
 {
-    class Program
+    public class ClockRadio
     {
-        static void Main(string[] args)
+        public int volumeRadio;
+        public int volumeAlarm;
+        public string timeCurrent;
+        public string timeAlarm;
+        public bool timeToWake;
+
+        public ClockRadio(int VolumeAlarm, string TimeCurrent, string TimeAlarm)
+
         {
+            volumeAlarm = VolumeAlarm;
+            timeCurrent = TimeCurrent;
+            timeAlarm = TimeAlarm;
+        }
+        public bool Awake()
+        {
+            bool result;
+            Console.WriteLine("Enter the current time: ");
+            string timeCurrent = Console.ReadLine();
+            if (timeCurrent == timeAlarm)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+        public int Alarm()
+        {
+            bool timeToWake = Awake();
+            if (timeToWake == true)
+            {
+                volumeAlarm = 10;
+            }
+            return volumeAlarm;
         }
     }
 }
